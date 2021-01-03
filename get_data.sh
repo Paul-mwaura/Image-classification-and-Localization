855e39672c852e1385781856608e67fc0a2a3210
Droid021 <vincivenv@gmail.com>
2021-01-03 20:52:56 +0000

    Setup

---
+++ get_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gdown "https://drive.google.com/uc?id=1KdpV3M27kV-_QOQOrAentfzZ2tew8YS-&"
+unzip -qq data/images.zip -d data
+rm -rf data/images.zip
---
+++ train.py
@@ -0,0 +1,3 @@
+random.seed(42)
+np.random.seed(42)
+torch.manual_seed(42)

d47d5672e3bd916a152df980aa1d9502c86c898b
Droid021 <vincivenv@gmail.com>
2021-02-28 14:09:23 +0300

    get data shell script and license

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir data
 gdown "https://drive.google.com/uc?id=1KdpV3M27kV-_QOQOrAentfzZ2tew8YS-&"
-unzip -qq data/images.zip -d data
-rm -rf data/images.zip
+unzip -qq images_fullsize.zip -d data
+rm -rf images_fullsize.zip
